86dd8ce127a27118ae6f7236694ed67f66f9f184
Sofie <sofie.kodehode@gmail.com>
2022-08-10 14:21:04 +0200

    placeholder profilepic

--- src/LandingPage.js/Me.js
+++ src/LandingPage.js/Me.js
@@ -1,65 +1,59 @@
-
-import  styled  from "styled-components";
+import styled from "styled-components";
 import React from "react";
-import PicMe from "./PicMe.png";
+import Queen from "./Queen.jpg";
 
+export const Column = styled.div`
+  display: flex;
+  padding-top: 500px;
+  flex-direction: row;
+  flex-wrap: nowrap;
 
-export const Column = styled.div `
-    display:flex;
-    padding-top: 500px;
-    flex-direction: row;
-    flex-wrap: nowrap;
-    
-    margin: 40 px;
-   
-`
-export const Image = styled.img `
-display: flex;
-flex-direction: row;
-height: 300px;
-width: 300px;
-border-radius: 50%;
+  margin: 40 px;
+`;
+export const Image = styled.img`
+  display: flex;
+  flex-direction: row;
+  height: auto;
+  width: 300px;
+  border-radius: 50%;
+`;
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
 
+  margin: 40px;
+`;
 
-`
-export const Container = styled.section `
-display: flex;
-flex-direction: column;
+export const NewSection = styled.section`
+  display: flex;
+  flex-direction: row;
+`;
 
-margin: 40px;
-
-`
-
-export const NewSection = styled.section `
-    display:flex;
-    flex-direction:row;
-    
-`
-
-const About = ()=> {
-    return(
-        <>
-
-        <Container>
+const About = () => {
+  return (
+    <>
+      <Container>
         <NewSection>
-        <Column>
-            
-                <Container>
-                    <h3>About</h3>
-                
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonumy eirmod tempor incididunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Ut enim ad minim veniam, quis nostrud exerception pariatur, sed diam voluptua et justo dolore magna aliquyam erat. Except id,sed diam nonumy eirmod tempor incididunt ut labore et dolore magna aliquyam erat.  </p>
-                </Container>
-                <Image src={PicMe} alt="me" />
-            </Column>
-            
-            </NewSection>
-           
-        </Container>
-            
-        </>
-        
-    )
-}
+          <Column>
+            <Container>
+              <h3>About</h3>
 
+              <p>
+                {" "}
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                diam nonumy eirmod tempor incididunt ut labore et dolore magna
+                aliquyam erat, sed diam voluptua. Ut enim ad minim veniam, quis
+                nostrud exerception pariatur, sed diam voluptua et justo dolore
+                magna aliquyam erat. Except id,sed diam nonumy eirmod tempor
+                incididunt ut labore et dolore magna aliquyam erat.{" "}
+              </p>
+            </Container>
+            <Image src={Queen} alt="me" />
+          </Column>
+        </NewSection>
+      </Container>
+    </>
+  );
+};
 
 export default About;
